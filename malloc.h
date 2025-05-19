0f577e96786c9b1ed279d6f1b7f846669f5223b2
DeshonS <deshonjewell@gmail.com>
2025-05-19 18:26:11 -0500

    added several blank lines

--- malloc.h
+++ malloc.h
@@ -32,6 +32,8 @@ struct meta_block
 	} align;
 };
 
+
+
 void *_malloc(size_t size);
 struct meta_block *request_space(struct meta_block *last, size_t size);
 struct meta_block *find_free_block(struct meta_block **last, size_t size);
