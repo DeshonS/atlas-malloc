6141e93b69fe4d7394d955ae5853aa601920b2c5
DeshonS <deshonjewell@gmail.com>
2025-05-19 17:33:36 -0500

    moved includes to top of file

--- malloc.h
+++ malloc.h
@@ -1,3 +1,9 @@
+#include <assert.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stddef.h>
+
 struct meta_block {
 	size_t size;
 	struct meta_block *next;
@@ -5,12 +11,6 @@ struct meta_block {
 };
 
 #define META_SIZE sizeof(struct meta_block)
-
-#include <assert.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stddef.h>
 
 void *_malloc(size_t size);
 struct meta_block *request_space(struct meta_block* last, size_t size);
